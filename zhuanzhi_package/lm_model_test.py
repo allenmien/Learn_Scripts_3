7efee9883e0c46c0b95b8a55744c36738a5d14ee
mark.ma <mark.ma@qingbao.cn>
2018-09-05 17:34:03 +0800

    测试字符级语言模型

---
+++ zhuanzhi_package/lm_model_test.py
@@ -0,0 +1,10 @@
+# -*-coding:utf-8-*-
+"""
+@Time   : 2018/9/5 17:22
+@Author : Mark
+@File   : lm_model_test.py
+"""
+from zhuanzhi_ner.data.embeddings import CharacterEmbeddings
+
+cahr_lm_embedding = CharacterEmbeddings(u'./resources/LM_model/lm-3.pt')
+pass
