e6efc01c258ec34486606724fd8da8c0c833cd3c
mark.ma <mark.ma@qingbao.cn>
2018-09-06 09:42:43 +0800

    测试字符级语言模型

--- zhuanzhi_package/lm_model_test.py
+++ zhuanzhi_package/lm_model_test.py
@@ -4,7 +4,16 @@
 @Author : Mark
 @File   : lm_model_test.py
 """
-from zhuanzhi_ner.data.embeddings import CharacterEmbeddings
+from zhuanzhi_ner.data import Sentence
+from zhuanzhi_ner.data.embeddings import CharLMEmbeddings
 
-cahr_lm_embedding = CharacterEmbeddings(u'./resources/LM_model/lm-3.pt')
+model_path = u'./resources/LM_model/lm-1.pt'
+
+# 实例例化CharLMEmbeddings类
+charlm_embedding_forward = CharLMEmbeddings(model_path)
+# 创建`Sentence`对象
+sentence = Sentence('国防集团 公司 .')
+# embed句句⼦子中的词
+sentence_embedding = charlm_embedding_forward.embed(sentence)
 pass
+
